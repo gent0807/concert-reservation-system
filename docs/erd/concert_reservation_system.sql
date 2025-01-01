a8f00ec586a9e27b58311fc258494b5234ae0b3a
gent0807 <theloopholesnk@gmail.com>
2025-01-01 19:07:46 +0900

    [Fix]: 상태 테이블 추가 생성
    - reservation 상태 테이블, reservation_statuses 생성
    - concert_detail_statuses 테이블, seat_statuses 테이블 생성

--- docs/erd/concert_reservation_system.sql
+++ docs/erd/concert_reservation_system.sql
@@ -20,10 +20,15 @@ CREATE TABLE `concert_detail_registers` (
   `updated_at` timestamp
 );
 
+CREATE TABLE `concert_detail_statuses` (
+  `concert_detail_status_id` integer PRIMARY KEY,
+  `concert_detail_status_name` varchar(255)
+);
+
 CREATE TABLE `concert_details` (
   `concert_detail_id` bigint PRIMARY KEY,
   `concert_basic_id` bigint UNIQUE,
-  `status` varchar(255) DEFAULT 'reservable',
+  `concert_detail_status_id` integer DEFAULT 1,
   `start_date` timestamp UNIQUE,
   `end_datee` timestamp UNIQUE,
   `concert_detail_register_id` varchar(255),
@@ -48,11 +53,16 @@ CREATE TABLE `casts` (
   `updated_at` timestamp
 );
 
+CREATE TABLE `seat_statuses` (
+  `seat_status_id` integer PRIMARY KEY,
+  `seat_status_name` varchar(255)
+);
+
 CREATE TABLE `seats` (
   `seat_id` bigint PRIMARY KEY,
   `concert_detail_id` bigint UNIQUE,
   `seat_number` bigint UNIQUE,
-  `status` varchar(255),
+  `seat_status_id` integer DEFAULT 1,
   `price` integer,
   `created_at` timestamp,
   `updated_at` timestamp
@@ -76,11 +86,16 @@ CREATE TABLE `waitings` (
   `deleted_at` timestamp
 );
 
+CREATE TABLE `reservation_statuses` (
+  `reservation_status_id` integer PRIMARY KEY,
+  `reservation_status_name` varchar(255)
+);
+
 CREATE TABLE `reservations` (
   `reservation_id` bigint PRIMARY KEY,
   `seat_id` bigint,
   `member_id` bigint,
-  `status` varchar(255),
+  `reservation_status_id` integer DEFAULT 1,
   `created_at` timestamp,
   `updated_at` timestamp
 );
@@ -91,14 +106,20 @@ ALTER TABLE `concert_details` ADD FOREIGN KEY (`concert_basic_id`) REFERENCES `c
 
 ALTER TABLE `concert_details` ADD FOREIGN KEY (`concert_detail_register_id`) REFERENCES `concert_detail_registers` (`concert_detail_register_id`);
 
+ALTER TABLE `concert_details` ADD FOREIGN KEY (`concert_detail_status_id`) REFERENCES `concert_detail_statuses` (`concert_detail_status_id`);
+
 ALTER TABLE `casts` ADD FOREIGN KEY (`concert_detail_id`) REFERENCES `concert_details` (`concert_detail_id`);
 
 ALTER TABLE `casts` ADD FOREIGN KEY (`arist_id`) REFERENCES `artists` (`artist_id`);
 
 ALTER TABLE `seats` ADD FOREIGN KEY (`concert_detail_id`) REFERENCES `concert_details` (`concert_detail_id`);
 
+ALTER TABLE `seats` ADD FOREIGN KEY (`seat_status_id`) REFERENCES `seat_statuses` (`seat_status_id`);
+
 ALTER TABLE `waitings` ADD FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`);
 
 ALTER TABLE `reservations` ADD FOREIGN KEY (`seat_id`) REFERENCES `seats` (`seat_id`);
 
 ALTER TABLE `reservations` ADD FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`);
+
+ALTER TABLE `reservations` ADD FOREIGN KEY (`reservation_status_id`) REFERENCES `reservation_statuses` (`reservation_status_id`);
