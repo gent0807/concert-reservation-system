9fcbb17c71b9ca1234a9840d1cfa8dd45d3af599
gent0807 <theloopholesnk@gmail.com>
2025-01-22 10:39:44 +0900

    [Config]: Testcontailners dependency 적용

--- src/test/java/io/dev/concertreservationsystem/application/reservation/concert/ConcertReservationConcurrencyTest.java
+++ src/test/java/io/dev/concertreservationsystem/application/reservation/concert/ConcertReservationConcurrencyTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ import java.util.concurrent.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 public class ConcertReservationConcurrencyTest {
     @Autowired
     ConcertReserveAdminFacade concertReserveAdminFacade;
