c3273335d8375bb87fa559b38a1e8f5fc4df8089
gent0807 <theloopholesnk@gmail.com>
2025-02-20 02:18:30 +0900

    [Feat]: Producer 구현체 생성, 이벤트 모델 생성

---
+++ src/main/java/io/dev/concertreservationsystem/infrastructure/common/producer/ProducerImpl.java
@@ -0,0 +1,18 @@
+package io.dev.concertreservationsystem.infrastructure.common.producer;
+
+import io.dev.concertreservationsystem.domain.common.producer.Producer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProducerImpl implements Producer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Override
+    public void produce(String topic, String key, String payload){
+        kafkaTemplate.send(topic, key, payload);
+    }
+}
