f911a8f9a67dd9365c866d09db858e891550eaa6
gent0807 <theloopholesnk@gmail.com>
2025-02-20 01:04:58 +0900

    [Refactor]: Kafka topic 이름 수정 및 추가
    - 이벤트 관련된 네이밍 방식으로 수정
    - 결제 관련 이벤트 topic 추가

--- src/main/java/io/dev/concertreservationsystem/common/config/kafka/KafkaKey.java
+++ src/main/java/io/dev/concertreservationsystem/common/config/kafka/KafkaKey.java
@@ -3,11 +3,17 @@ package io.dev.concertreservationsystem.common.config.kafka;
 public class KafkaKey {
     public static final String CONSUMER_GROUP_ID = "CONSUMER_GROUP_ID";
 
-    public static final String CONCERT_BASIC_TOPIC = "CONCERT_BASIC_TOPIC";
+    public static final String CONCERT_BASIC_EVENT = "CONCERT_BASIC_EVENT";
 
-    public static final String CONCERT_DETAIL_TOPIC = "CONCERT_DETAIL_TOPIC";
+    public static final String CONCERT_DETAIL_STATUS_UPDATE_EVENT = "CONCERT_DETAIL_STATUS_UPDATE_EVENT";
 
-    public static final String SEAT_TOPIC = "SEAT_TOPIC";
+    public static final String SEAT_STATUS_UPDATE_EVENT = "SEAT_STATUS_UPDATE_EVENT";
 
-    public static final String RESERVATION_TOPIC = "RESERVATION_TOPIC";
+    public static final String RESERVATION_CREATE_EVENT = "RESERVATION_CREATE_EVENT";
+
+    public static final String RESERVATION_UPDATE_EVENT = "RESERVATION_UPDATE_EVENT";
+
+    public static final String PAYMENT_CREATE_EVENT = "PAYMENT_CREATE_EVENT";
+
+    public static final String PAYMENT_UPDATE_EVENT = "PAYMENT_UPDATE_EVENT";
 }
